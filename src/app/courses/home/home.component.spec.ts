8b60fe7e9ef524c425c6e2fca841d84a9b4573c9
Shrey Baxi <coder.shrey@gmail.com>
2023-06-04 22:20:03 +0530

    Home component done

---
+++ src/app/courses/home/home.component.spec.ts
@@ -0,0 +1,111 @@
+import { DebugElement } from "@angular/core";
+import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { CoursesModule } from "../courses.module";
+
+import { By } from "@angular/platform-browser";
+import { of } from "rxjs";
+import { setupCourses } from "../common/setup-test-data";
+import { click } from "../common/test-utils";
+import { CoursesService } from "../services/courses.service";
+import { HomeComponent } from "./home.component";
+
+describe("HomeComponent", () => {
+  let fixture: ComponentFixture<HomeComponent>;
+  let component: HomeComponent;
+  let el: DebugElement;
+  let coursesService: any;
+
+  const beginnerCourses = setupCourses().filter(
+    (c) => c.category === "BEGINNER"
+  );
+
+  const advancedCourses = setupCourses().filter(
+    (c) => c.category === "ADVANCED"
+  );
+
+  beforeEach(waitForAsync(() => {
+    const coursesServiceSpy = jasmine.createSpyObj("CoursesService", [
+      "findAllCourses",
+    ]);
+    TestBed.configureTestingModule({
+      imports: [CoursesModule, NoopAnimationsModule],
+      providers: [{ provide: CoursesService, useValue: coursesServiceSpy }],
+    })
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(HomeComponent);
+        component = fixture.componentInstance;
+        el = fixture.debugElement;
+        coursesService = TestBed.inject(CoursesService);
+      });
+  }));
+
+  it("should create the component", () => {
+    expect(component).toBeTruthy();
+  });
+
+  it("should display only beginner courses", () => {
+    coursesService.findAllCourses.and.returnValue(of(beginnerCourses));
+
+    fixture.detectChanges();
+
+    const tabs = el.queryAll(By.css(".mdc-tab"));
+    expect(tabs.length).toBe(1, "Unexpected number of tabs found");
+
+    const label = tabs[0].query(By.css(".mdc-tab__text-label"));
+    expect(label.nativeElement.innerText).toBe(
+      "Beginners",
+      "Unexpected tab label found"
+    );
+  });
+
+  it("should display only advanced courses", () => {
+    coursesService.findAllCourses.and.returnValue(of(advancedCourses));
+
+    fixture.detectChanges();
+
+    const tabs = el.queryAll(By.css(".mdc-tab"));
+    expect(tabs.length).toBe(1, "Unexpected number of tabs found");
+
+    const label = tabs[0].query(By.css(".mdc-tab__text-label"));
+    expect(label.nativeElement.innerText).toBe(
+      "Advanced",
+      "Unexpected tab label found"
+    );
+  });
+
+  it("should display both tabs", () => {
+    coursesService.findAllCourses.and.returnValue(of(setupCourses()));
+
+    fixture.detectChanges();
+
+    const tabs = el.queryAll(By.css(".mdc-tab"));
+    expect(tabs.length).toBe(2, "Unexpected number of tabs found");
+  });
+
+  it("should display advanced courses when tab clicked", (done: DoneFn) => {
+    coursesService.findAllCourses.and.returnValue(of(setupCourses()));
+
+    fixture.detectChanges();
+    const tabs = el.queryAll(By.css(".mdc-tab"));
+
+    click(tabs[1]);
+    fixture.detectChanges();
+
+    setTimeout(() => {
+      const cardTitles = el.queryAll(
+        By.css(".mat-mdc-tab-body-active .mat-mdc-card-title")
+      );
+      expect(cardTitles.length).toBeGreaterThan(
+        0,
+        "Could not find card titles"
+      );
+      expect(cardTitles[0].nativeElement.textContent).toContain(
+        "Angular Security Course",
+        "Advanced courses are not displayed"
+      );
+      done();
+    }, 500);
+  });
+});
